e36a955d134739828a411058a1750484db023ac3
RomanTuras <salden.com@gmail.com>
2024-11-17 12:19:52 +0200

    Add first task

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
-import './App.css'
+import Container from './components/container/Container';
+import Profile from './components/profile/Profile';
+import userData from "./userData.json";
 
 function App() {
   return (
-    <>
-      <p>hello morda!</p>
-    </>
+    <Container>
+       <Profile
+        name={userData.username}
+        tag={userData.tag}
+        location={userData.location}
+        image={userData.avatar}
+        stats={userData.stats}
+      />
+    </Container>
   )
 }
 
